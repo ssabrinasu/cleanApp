0d162f9dcb50c30d972d37418a3b1e6074e66ad0
Sabrina <ssabrinasutili@gmail.com>
2021-09-23 15:10:16 -0300

    Add CI scheme

--- InfraTests/New Group/AlamofireAdapterTests.swift
+++ InfraTests/New Group/AlamofireAdapterTests.swift
@@ -8,41 +8,7 @@
 import XCTest
 import Alamofire
 import Data
-
-class AlamofireAdapter: HttpPostClient {
-    private let session: Session
-    
-    init(session: Session = .default) {
-        self.session = session
-    }
-
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
-            guard let statusCode = dataResponse.response?.statusCode else { return completion(.failure(.noConnectivity)) }
-            switch dataResponse.result {
-            case .failure: completion(.failure(.noConnectivity))
-            case .success(let data):
-                switch statusCode {
-                case 204:
-                    completion(.success(nil))
-                case 200...299:
-                    completion(.success(data))
-                case 401:
-                    completion(.failure(.unauthorized))
-                case 403:
-                    completion(.failure(.forbidden))
-                case 400...499:
-                    completion(.failure(.badRequest))
-                case 500...599:
-                    completion(.failure(.serverError))
-                default:
-                    completion(.failure(.noConnectivity))
-                }
-            }
-        }
-        
-    }
-}
+import Infra
 
 class AlamofireAdapterTests: XCTestCase {
 
@@ -107,6 +73,9 @@ class AlamofireAdapterTests: XCTestCase {
         
         expectResult(.failure(.unauthorized), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 401), error: nil))
         expectResult(.failure(.forbidden), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 403), error: nil))
+        
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 100), error: nil))
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 300), error: nil))
     }
 
 }
@@ -147,44 +116,4 @@ extension AlamofireAdapterTests {
     }
 }
 
-class UrlProtocolStub: URLProtocol {
-    static var emit: ((URLRequest) -> Void)?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    static var error: Error?
-    
-    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
-        UrlProtocolStub.emit = completion
-    }
-    
-    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
-        UrlProtocolStub.data = data
-        UrlProtocolStub.response = response
-        UrlProtocolStub.error = error
-    }
-    
-    override open class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
 
-    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-
-    override open func startLoading() {
-        UrlProtocolStub.emit?(request)
-        if let data = UrlProtocolStub.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-        if let response = UrlProtocolStub.response {
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-        if let error = UrlProtocolStub.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-        client?.urlProtocolDidFinishLoading(self)
-    }
-
-    override open func stopLoading() {}
-    
-}
