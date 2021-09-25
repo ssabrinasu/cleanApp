28f58ce4e415ac1e2646676430b4ffecdc00cadd
Sabrina <ssabrinasutili@gmail.com>
2021-09-24 22:14:54 -0300

    Add presentation layer

---
+++ PresentationTests/SingUpPresenterTests.swift
@@ -0,0 +1,16 @@
+//
+//  SingUpPresenterTests.swift
+//  PresentationTests
+//
+//  Created by Sabrina on 24/09/21.
+//
+
+import XCTest
+
+class SingUpPresenterTests: XCTestCase {
+
+    func test_() throws {
+        
+    }
+
+}
---
+++ UseCasesIntegrationTests/UseCasesIntegrationTests.swift
@@ -0,0 +1,32 @@
+//
+//  UseCasesIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Sabrina on 23/09/21.
+//
+
+//import XCTest
+//import Data
+//import Infra
+//import Domain
+//
+//class AddAccountIntegrationTests: XCTestCase {
+//    func test_add_account() {
+//        let alamofireAdapter = AlamofireAdapter()
+//        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
+//        let sut = RemoteAddAccount(url: url, HttpClient: alamofireAdapter)
+//        let addAccountModel = AddAccountModel(name: "Rodrigo Manguinho", email: "rodrigo.manguinho@gmail.com", password: "secret", passwordConfiemation: "secret")
+//        let exp = expectation(description: "waiting")
+//        sut.add(addAccountModel: addAccountModel) { result in
+//            switch result {
+//            case .failure: XCTFail("Expect success got \(result) instead")
+//            case .success(let account):
+//                XCTAssertNotNil(account.id)
+//                XCTAssertEqual(account.name, addAccountModel.name)
+//                XCTAssertEqual(account.email, addAccountModel.email)
+//            }
+//            exp.fulfill()
+//        }
+//        wait(for: [exp], timeout: 5)
+//    }
+//}
