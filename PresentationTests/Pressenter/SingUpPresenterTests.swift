6dea1dddfb829ded195adbf9b639921972080895
Sabrina <ssabrinasutili@gmail.com>
2021-09-28 14:44:06 -0300

    Move dublicated code to a helper

--- PresentationTests/Pressenter/SingUpPresenterTests.swift
+++ PresentationTests/Pressenter/SingUpPresenterTests.swift
@@ -12,7 +12,7 @@ class SingUpPresenterTests: XCTestCase {
     func test_singUp_should_show_error_message_if_name_is_not_provided() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let singUpViewModel = SingUpViewModel(email: "any@email.com", password: "any12345", passwordConfirmation: "any12345")
+        let singUpViewModel = makeSingUpViewModel(name: nil)
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatorio"))
     }
@@ -20,7 +20,7 @@ class SingUpPresenterTests: XCTestCase {
     func test_singUp_should_show_error_message_if_email_is_not_provided() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let singUpViewModel = SingUpViewModel(name: "Any Name", password: "any12345", passwordConfirmation: "any12345")
+        let singUpViewModel = makeSingUpViewModel(email: nil)
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Email é obrigatorio"))
     }
@@ -28,7 +28,7 @@ class SingUpPresenterTests: XCTestCase {
     func test_singUp_should_show_error_message_if_password_is_not_provided() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let singUpViewModel = SingUpViewModel(name: "Any Name", email: "any@email.com", passwordConfirmation: "any12345")
+        let singUpViewModel = makeSingUpViewModel(password: nil)
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Senha é obrigatorio"))
     }
@@ -36,7 +36,7 @@ class SingUpPresenterTests: XCTestCase {
     func test_singUp_should_show_error_message_if_password_confirmation_is_not_match() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let singUpViewModel = SingUpViewModel(name: "Any Name", email: "any@email.com", password: "any12345", passwordConfirmation: "any")
+        let singUpViewModel = makeSingUpViewModel(passwordConfirmation: "Any")
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Falha ao confirmar senha"))
     }
@@ -44,7 +44,7 @@ class SingUpPresenterTests: XCTestCase {
     func test_singUp_should_show_emailValidator_with_correct_email() {
         let emailValidatorSpy = EmailValidatorSpy()
         let sut = makeSut(emailValidator: emailValidatorSpy)
-        let singUpViewModel = SingUpViewModel(name: "Any Name", email: "any@email.com", password: "any12345", passwordConfirmation: "any12345")
+        let singUpViewModel = makeSingUpViewModel()
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(emailValidatorSpy.email, singUpViewModel.email)
     }
@@ -53,7 +53,7 @@ class SingUpPresenterTests: XCTestCase {
         let alertViewSpy = AlertViewSpy()
         let emailValidatorSpy = EmailValidatorSpy()
         let sut = makeSut(alertView: alertViewSpy, emailValidator: emailValidatorSpy)
-        let singUpViewModel = SingUpViewModel(name: "Any Name", email: "any@email.com", password: "any12345", passwordConfirmation: "any12345")
+        let singUpViewModel = makeSingUpViewModel()
         emailValidatorSpy.isValid = false
         sut.singUp(viewModel: singUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Email inválido"))
@@ -64,6 +64,11 @@ extension SingUpPresenterTests {
     func makeSut(alertView: AlertViewSpy = AlertViewSpy(), emailValidator: EmailValidatorSpy = EmailValidatorSpy()) -> SingUpPresenter {
         let sut = SingUpPresenter(alertView: alertView, emailValidator: emailValidator)
         return sut
+    }
+    
+    func makeSingUpViewModel(name: String? = "Any Name", email: String? = "any@email.com", password: String? = "any12345", passwordConfirmation: String? = "any12345") -> SingUpViewModel {
+        return SingUpViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation)
+        
     }
     
     class AlertViewSpy: AlertView {
