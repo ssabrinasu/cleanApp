9f11aed56600b7289bfe7aa9fe4036151c639b5f
Sabrina <ssabrinasutili@gmail.com>
2021-09-30 16:36:47 -0300

    Add Validator target

---
+++ ValidationTests/EmailValidatorAdapter.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidatorAdapter.swift
+//  ValidationTests
+//
+//  Created by Sabrina on 30/09/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapter: XCTestCase {
+
+    func testExample() {
+     
+    }
+}
