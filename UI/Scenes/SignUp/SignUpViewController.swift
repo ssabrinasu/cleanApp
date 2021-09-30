07238ea64cf718c97f0f5410c08924ceedb30b70
Sabrina <ssabrinasutili@gmail.com>
2021-09-30 15:24:45 -0300

    Make SignUp layout

--- UI/Scenes/SignUp/SignUpViewController.swift
+++ UI/Scenes/SignUp/SignUpViewController.swift
@@ -26,6 +26,7 @@ final class SignUpViewController: UIViewController {
     }
     
     func configure() {
+        saveButton?.layer.cornerRadius = 5
         saveButton?.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
     
