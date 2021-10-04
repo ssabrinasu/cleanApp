080c7d6aed2c7b8856687774c814a2df2c469be3
Sabrina <ssabrinasutili@gmail.com>
2021-10-04 10:41:58 -0300

    Remove ControllerFactory

--- Main/Factories/ControllerFactory.swift
+++ Main/Factories/ControllerFactory.swift
@@ -7,18 +7,13 @@
 
 import Foundation
 import UI
-import Presentation
-import Validation
+
 import Data
 import Infra
 import Domain
 
 class ControllerFactory {
     static func makeController(addAccount: AddAccount) -> SignUpViewController {
-        let controller = SignUpViewController.instantiate()
-        let emailValidatorAdapter = EmailValidatorAdapter()
-        let presenter = SingUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
-        controller.signUp = presenter.singUp
-        return controller
+        
     }
 }
