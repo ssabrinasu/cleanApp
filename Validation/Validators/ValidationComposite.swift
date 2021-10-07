37a6ffdc752b3b27e4f9a57b73935eaebce7b0c9
Sabrina <ssabrinasutili@gmail.com>
2021-10-07 16:19:41 -0300

    Move validation to the Main Layer using the Composite Pattern

---
+++ Validation/Validators/ValidationComposite.swift
@@ -0,0 +1,8 @@
+//
+//  ValidationComposite.swift
+//  Validation
+//
+//  Created by Sabrina on 07/10/21.
+//
+
+import Foundation
---
+++ ValidationTests/Protocols/ValidationSpy.swift
@@ -0,0 +1,8 @@
+//
+//  ValidationSpy.swift
+//  ValidationTests
+//
+//  Created by Sabrina on 07/10/21.
+//
+
+import Foundation
