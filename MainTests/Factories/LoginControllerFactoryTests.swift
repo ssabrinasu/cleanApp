45f7ba8e62ce62c677bba12934cb831cd17f21bb
Sabrina <ssabrinasutili@gmail.com>
2021-10-12 15:30:23 -0300

    Ensure LoginController comopose with correct Validations

---
+++ MainTests/Factories/LoginControllerFactoryTests.swift
@@ -0,0 +1,43 @@
+//
+//  LoginControllerFactoryTests.swift
+//  MainTests
+//
+//  Created by Sabrina on 09/10/21.
+//
+import XCTest
+import Main
+import UI
+import Validation
+
+class LoginControllerFactoryTests: XCTestCase {
+//    func test_background_request_should_complete_on_main_thread() {
+//        let (sut, authenticationSpy) = makeSut()
+//        sut.loadViewIfNeeded()
+//        sut.signUp?(makeSingUpViewModel())
+//        let exp = expectation(description: "waiting")
+//        DispatchQueue.global().async {
+//            authenticationSpy.completedWithError(.unexpected)
+//            exp.fulfill()
+//        }
+//        wait(for: [exp], timeout: 1)
+//    }
+    
+    func test_singUp_compose_with_correct_validations() {
+        let validations = makeLoginValidations()
+        XCTAssertEqual(validations[0] as! RequiredFieldValidation, RequiredFieldValidation(fieldName: "email", fieldLabel: "Email"))
+        XCTAssertEqual(validations[1] as! EmailValidation, EmailValidation(fieldName: "email", fieldLabel: "Email", emailValidator: EmailValidatorSpy()))
+        XCTAssertEqual(validations[2] as! RequiredFieldValidation, RequiredFieldValidation(fieldName: "password", fieldLabel: "Senha"))
+    }
+}
+
+extension LoginControllerFactoryTests {
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: LoginViewController, authenticationSpy: AuthenticationSpy) {
+        let authenticationSpy = AuthenticationSpy()
+        let sut = makeLoginController(authentication: authenticationSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: authenticationSpy, file: file, line: line)
+        return (sut, authenticationSpy)
+    }
+}
+
+
