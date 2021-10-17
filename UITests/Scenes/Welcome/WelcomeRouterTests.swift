21c87cec873c32c5be0e0fb5d978e2db9498d871
Sabrina <ssabrinasutili@gmail.com>
2021-10-16 21:49:13 -0300

    Move SUT creation to a factory method

--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -27,19 +27,29 @@ class WelcomeRouter {
 
 class WelcomeRouterTests: XCTestCase {
     func test_goToLogin_calls_nav_with_correct_vc()  {
-        let loginFactorySpy = LoginFactorySpy()
-        let nav = NavigationController()
-        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin)
+        let (sut, nav) = makeSut()
         sut.goToLogigin()
         XCTAssertEqual(nav.viewControllers.count, 1)
         XCTAssertTrue(nav.viewControllers[0] is LoginViewController)
     }
     
+    
+}
+
+extension WelcomeRouterTests {
+    func makeSut() -> (sut: WelcomeRouter, nav: NavigationController) {
+        let loginFactorySpy = LoginFactorySpy()
+        let nav = NavigationController()
+        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin)
+        return (sut, nav)
+    }
+}
+
+extension WelcomeRouterTests {
     class LoginFactorySpy {
         func makeLogin()-> LoginViewController {
             return LoginViewController.instantiate()
         }
     }
 }
-
 
